ced21057f543305605b00acbdb5d68def2315c59
leandrohorizon <leandro.mcontato@gmail.com>
2021-10-03 10:37:13 -0300

    Utiliza fabricator

--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Wallet, type: :model do
   context 'Cria carteira dos cria' do
-    let(:account) { Fabricate(:account) }
+    let(:wallet) { Fabricate(:wallet) }
 
     it 'com parâmetros válidos' do
-      wallet = Wallet.new(source: 'banco friba',
-                          amount_cents: 1000,
-                          account: account)
-
       expect(wallet).to be_valid
     end
   end
