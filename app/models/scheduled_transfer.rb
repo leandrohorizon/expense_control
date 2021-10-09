43bd99e63c13f908d9d06a1df2c55419011488ca
leandrohorizon <leandro.mcontato@gmail.com>
2021-10-09 07:52:35 -0300

    Adiciona opção para frequency

--- app/models/scheduled_transfer.rb
+++ app/models/scheduled_transfer.rb
@@ -1,4 +1,4 @@
 class ScheduledTransfer < ApplicationRecord
   enum occurrence_type: %w[credit debit]
-  enum frequency: %w[unique daily monthly yearly]
+  enum frequency: %w[unique daily monthly yearly forever]
 end
