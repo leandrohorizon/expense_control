8c9347cb7550cadeed834e3ddf66eca2a6f281c2
leandrohorizon <leandro.mcontato@gmail.com>
2021-10-09 10:55:29 -0300

    Adiciona metodos next_payment e paid!

--- app/models/scheduled_transfer.rb
+++ app/models/scheduled_transfer.rb
@@ -1,6 +1,32 @@
+# frozen_string_literal: true
+
 class ScheduledTransfer < ApplicationRecord
   has_many :historic
 
   enum occurrence_type: %w[credit debit]
-  enum frequency: %w[unique daily weekly monthly yearly forever]
+  enum frequency: %w[unique daily weekly monthly yearly]
+
+  def next_payment
+    return false if unique?
+
+    case frequency
+    when 'daily' then scheduled_date + 1.day
+    when 'weekly' then puts scheduled_date + 7.day
+    when 'monthly' then scheduled_date + 1.month
+    when 'yearly' then scheduled_date + 1.year
+    else
+      raise 'invalid frequency'
+    end
+  end
+
+  def paid!(wallet, adjustment = 0)
+    return if quantity&.zero?
+
+    decrement!(:quantity) unless quantity.nil?
+
+    historic.create(description: description,
+                    amount_cents: amount_cents + adjustment,
+                    occurrence_type: occurrence_type,
+                    wallet: wallet)
+  end
 end
