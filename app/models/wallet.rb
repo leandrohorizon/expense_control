de22c206d8be0f50ca360a88616b352923d96274
leandrohorizon <leandro.mcontato@gmail.com>
2022-05-28 18:10:44 -0300

    Adiciona método amount

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -3,4 +3,18 @@ class Wallet < ApplicationRecord
   has_many :historics
 
   monetize :amount_cents
+
+  def amount
+    profit = Money.new(profits.sum(:amount_cents), 'BRL')
+    expense = Money.new(expenses.sum(:amount_cents), 'BRL')
+    profit - expense
+  end
+
+  def profits
+    historics.where(occurrence_type: :credit)
+  end
+
+  def expenses
+    historics.where(occurrence_type: :debit)
+  end
 end
